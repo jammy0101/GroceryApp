f9d000b10abbb6ba2747d836327567887631b829
jammy0101 <walker0101walker.gmail.com>
2025-06-01 12:41:22 +0500

    Updated splash image

--- lib/splashScreen/splashScreen.dart
+++ lib/splashScreen/splashScreen.dart
@@ -1,5 +1,40 @@
-
-import 'package:flutter/cupertino.dart';
+//
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import '../splashServices/splashServices.dart';
+//
+// class Splashscreen extends StatefulWidget {
+//   const Splashscreen({super.key});
+//
+//   @override
+//   State<Splashscreen> createState() => _SplashscreenState();
+// }
+//
+// SplashService  splashService = SplashService();
+//
+// class _SplashscreenState extends State<Splashscreen> {
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     splashService.isLogin(context);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return  const Scaffold(
+//       body: Column(
+//
+//         children: [
+//           Image(
+//             fit: BoxFit.fill,
+//               image: ExactAssetImage('assets/images/super2.jpg')),
+//         ],
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import '../splashServices/splashServices.dart';
 
@@ -10,26 +45,23 @@ class Splashscreen extends StatefulWidget {
   State<Splashscreen> createState() => _SplashscreenState();
 }
 
-SplashService  splashService = SplashService();
-
 class _SplashscreenState extends State<Splashscreen> {
+  final SplashService splashService = SplashService();
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     splashService.isLogin(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(child: Text('My project',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.black),)),
-        ],
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Image.asset(
+          'assets/images/super.jpg',
+          fit: BoxFit.cover, // covers full screen
+        ),
       ),
     );
   }
